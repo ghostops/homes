b549e1c65d672ed7babf1f841d193d5a45ad4aca
ghostops <ludvig@ghostops.nu>
2020-06-02 11:38:54 +0200

    cleanup

--- Backend/src/database/database.go
+++ Backend/src/database/database.go
@@ -23,19 +23,6 @@ func ConnectDb() {
 	// Migrate the schema
 	db.AutoMigrate(&models.Home{})
 
-	// // Create
-	// db.Create(&Product{Code: "L1212", Price: 1000})
-
-	// // Read
-	// var product Product
-	// db.First(&product, 1)                   // find product with id 1
-	// db.First(&product, "code = ?", "L1212") // find product with code l1212
-
-	// // Update - update product's price to 2000
-	// db.Model(&product).Update("Price", 2000)
-
-	// // Delete - delete product
-	// db.Delete(&product)
-
+	// Set the global Database
 	Database = db
 }
