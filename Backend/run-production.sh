ae3a3bd125b3fdfa0d980ded6dfdd4a92960e506
ghostops <ludvig@ghostops.nu>
2020-06-16 16:33:44 +0200

    update runscript

--- Backend/run-production.sh
+++ Backend/run-production.sh
@@ -1,4 +1,6 @@
 HOMEAPP_PORT=4001
+HOMEAPP_NAME=homeapp
 
-docker build -t homeapp .
-docker run -d -p "$HOMEAPP_PORT:8080" homeapp
+docker build -t $HOMEAPP_NAME .
+docker kill $HOMEAPP_NAME
+docker run -d -p "$HOMEAPP_PORT:8080" --name $HOMEAPP_NAME $HOMEAPP_NAME
