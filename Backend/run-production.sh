a65941b0cbccf39962837498df59d06549bf0f46
ghostops <ludvig@ghostops.nu>
2020-06-16 16:36:19 +0200

    rm

--- Backend/run-production.sh
+++ Backend/run-production.sh
@@ -4,5 +4,5 @@ HOMEAPP_PORT=4001
 HOMEAPP_NAME=homeapp
 
 docker build -t $HOMEAPP_NAME .
-docker kill $HOMEAPP_NAME
+docker rm $HOMEAPP_NAME
 docker run -d -p "$HOMEAPP_PORT:8080" --name $HOMEAPP_NAME $HOMEAPP_NAME
