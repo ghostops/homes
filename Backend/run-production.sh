d31908e1eb482bedbb7a504ea5999e68c1989e2c
ghostops <ludvig@ghostops.nu>
2020-06-17 16:02:29 +0200

    run production fixes

--- Backend/run-production.sh
+++ Backend/run-production.sh
@@ -7,9 +7,10 @@ docker build -t $HOMEAPP_NAME .
 
 if [ ! "$(docker ps -q -f name=$HOMEAPP_NAME)" ]; then
     docker stop $HOMEAPP_NAME
-    if [ "$(docker ps -aq -f status=exited -f name=$HOMEAPP_NAME)" ]; then
-        docker rm $HOMEAPP_NAME
-    fi
+fi
+
+if [ "$(docker ps -aq -f status=exited -f name=$HOMEAPP_NAME)" ]; then
+    docker rm $HOMEAPP_NAME
 fi
 
 docker run -d -p "$HOMEAPP_PORT:8080" --name $HOMEAPP_NAME $HOMEAPP_NAME
